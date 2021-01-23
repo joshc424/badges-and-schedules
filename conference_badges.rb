ae9e4d533df845a173d9e1b1d243f7a876359ac8
Joshua Cho <joshdavecho@protonmail.com>
2021-01-23 13:49:40 -0500

    commit tree 96f8c369cf51b2d2c6ed69320ad989d549322dc5

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    names = []
+    array.each {|x| names << "Hello, my name is #{x}."}
+    return names
+end
+
+def assign_rooms(array)
+    names = []
+    array.each_with_index { |x, index| names << "Hello, #{x}! You'll be assigned to room #{index + 1}!"}
+    return names
+end
+
+def printer(array)
+    batch_badge_creator(array).each { |x| puts x}
+    assign_rooms(array).each { |x| puts x}
+end
+
